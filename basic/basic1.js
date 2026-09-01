fdd1857635991008439795bc4b63f0110d9fc3b0
Aashiq singh <108392697+Aashiqsingh@users.noreply.github.com>
2026-09-01 15:45:24 +0530

    introduction , datatypes , scope variable

---
+++ basic/basic1.js
@@ -0,0 +1,34 @@
+// console.log("hello")
+// console.info("information function..");
+// console.warn("warning function..");
+// console.error("error function..");
+
+
+// Datatype : number , string , boolean , undefined , object
+
+a = 5;
+console.log("value of a = ",a);
+// console.log("value of a = "+a);
+console.log("type of a = ",typeof(a));
+
+b = 5.55;
+console.log("value of b = ",b);
+console.log("type of b = ",typeof(b));
+
+name = "p";
+console.log("value of name = ",name);
+console.log("type of name = ",typeof(name));
+
+isMarried = true;
+console.log("value of isMarried = ",isMarried);
+console.log("type of isMarried = ",typeof(isMarried));
+
+var x;
+console.log("value of x = ",x);
+console.log("type of x = ",typeof x);
+
+
+y = null;
+console.log("value of y = ",y);
+console.log("type of y = ",typeof y);
+
---
+++ basic/basic2.js
@@ -0,0 +1,82 @@
+// var a = 10;
+// console.log("value of a = ",a);
+
+
+// // var a = 20;
+// a = 20;
+// console.log("value of a = ",a);
+
+
+// let a = 20;
+// console.log("value of a = ",a);
+
+// a = 30;
+// console.log("value of a = ",a);
+
+
+// const a = 20;
+// console.log("value of a = ",a);
+
+// a = 30;
+// console.log("value of a = ",a);
+
+// const a;
+// a = 10;
+
+// var a = 10;
+// console.log("outer value of a = ",a);
+
+// let b = 20;
+// console.log("outer value of b = ",b);
+
+// {
+//     console.log("inner value of a = ",a);
+//     console.log("inner value of b = ",b);
+    
+// }
+
+
+
+
+// {
+//     var a = 10;
+//     console.log("inner value of a=  ",a);
+
+//     let b = 20;
+//     console.log("inner value of b = ",b);
+
+//     {
+//         console.log("inner inner value of a = ",a);
+//         console.log(" inner inner value of b = ",b);
+//     }
+// }
+
+// console.log("outer value of a = ",a);
+// console.log("outer value of b = ",b);
+
+
+// const a = 10;
+// console.log("outer value of a = ",a);
+
+// {
+//     console.log("inner valu eof a = ",a);
+
+//     const pi = 3.14;
+//     console.log("inner value of pi = ",pi);
+    
+// }
+// console.log("outer value of pi = ",pi);
+
+var a = 30;
+{
+    let a = 10;
+    console.log("value of a = ",a);
+    
+}
+
+{
+    var a = 20;
+    console.log("value of a = ",a);
+}
+
+console.log("outer value of a = ",a);
